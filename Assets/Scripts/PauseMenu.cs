fc5de0fe48a8a0e51ffa8a989d01f0060deee434
MaxKennedy1196 <maxkennedy2767@gmail.com>
2025-04-21 19:49:21 +1000

    Pause Menu
    added the ability to pause the game:
    - players can choose to resume or to exit to the main menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
+
+    public bool isPaused;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    public void togglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
