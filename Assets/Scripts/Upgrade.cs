c7e367bc25873f6a4236457f4950c275dbb5f255
Asher Johnston <ashervjohnston@gmail.com>
2025-04-22 22:17:45 +1000

    Upgrade System WIP

---
+++ Assets/Scripts/Upgrade.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour
+{
+    [Serializable]
+    struct UpgradeValues
+    {
+        public bool projectileIncrease = false;
+        public bool attackRateIncrease = false;
+        public bool damageIncrease = false;
+        public bool healthIncrease = false;
+        public bool hungerIncrease = false;
+        public bool movementSpeedIncrease = false;
+    }
+
+    [SerializeField] UpgradeValues upgradeValues;
+
+    void ApplyUpgrade()
+    {
+        foreach (bool upgrade in upgradeValues)
+        {
+            if (upgrade) { ; }
+        }
+
+    }
+}
