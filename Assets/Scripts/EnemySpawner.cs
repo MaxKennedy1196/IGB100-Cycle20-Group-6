fed961129c1436f7281d3d04e85663ffb7efc081
EasilyDistractedInteractive <163216751+EasilyDistractedInteractive@users.noreply.github.com>
2025-05-13 12:44:31 +1000

    Spawn mods + txt mods
    Spawn mods + txt mods

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -32,7 +32,7 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(spawningOn)
+        if(spawningOn && Manager.enemyList.Count < 250)
         {
             SpawnEnemy();
         }
