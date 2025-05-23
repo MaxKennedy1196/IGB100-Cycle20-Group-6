c2605996dbebd03ee520e570ec719bb835b3be13
Asher Johnston <ashervjohnston@gmail.com>
2025-05-24 01:00:45 +1000

    Added cutscene system
    Haven't written lines or fine tuned text positioning cause it's too late for that I don't have time

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -8,22 +8,24 @@ public class mainMenu : MonoBehaviour
     private AsyncOperation loadSceneOperation; //Asynchronous operation to preload the next scene.
 
     public SceneFade fadeOut;
+    public float fadeDuration;
     public AnimationCurve startFadeCurve;
     public AnimationCurve endFadeCurve;
+    public AudioSource menuMusic;
+
+    public Cutscene startCutscene;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        fadeOut.fadeDuration = fadeDuration;
         fadeOut.fadeCurve = startFadeCurve;
         fadeOut.ActivateFade();
-        //loadSceneOperation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
-        //loadSceneOperation.allowSceneActivation = false;
     }
     
     public void PlayGame()
     {
-        StartCoroutine(LoadNewScene(1));
-        //StartCoroutine(LoadGame());
+        startCutscene.Display();
     }
 
     // Update is called once per frame
@@ -37,22 +39,18 @@ public class mainMenu : MonoBehaviour
         StartCoroutine(LoadNewScene(0));
     }
 
-    public void Restart()
+    public void RunGame()
     {
         StartCoroutine(LoadNewScene(1));
     }
 
-    public IEnumerator LoadGame()
-    {
-        fadeOut.fadeCurve = endFadeCurve;
-        fadeOut.ActivateFade(); //Activating the screen fade and waiting out it's duration then activating the game scene
-        yield return new WaitForSeconds(fadeOut.fadeDuration + fadeOut.endWait);
-        //loadSceneOperation.allowSceneActivation = true;
-    }
+    public bool CutsceneComplete() { return startCutscene.cutsceneComplete; }
 
-    private IEnumerator LoadNewScene(int sceneNum)
+    public IEnumerator LoadNewScene(int sceneNum)
     {
+        fadeOut.fadeDuration = fadeDuration;
         fadeOut.fadeCurve = endFadeCurve;
+        fadeOut.music = menuMusic;
         fadeOut.ActivateFade(); //Activating the screen fade and waiting out it's duration then activating the game scene
         yield return new WaitForSeconds(fadeOut.fadeDuration + fadeOut.endWait);
         SceneManager.LoadScene(sceneNum);
@@ -60,7 +58,9 @@ public class mainMenu : MonoBehaviour
 
     private IEnumerator FadeAndQuit()
     {
+        fadeOut.fadeDuration = fadeDuration;
         fadeOut.fadeCurve = endFadeCurve;
+        fadeOut.music = menuMusic;
         fadeOut.ActivateFade();
         yield return new WaitForSeconds(fadeOut.fadeDuration + fadeOut.endWait);
         Application.Quit();
